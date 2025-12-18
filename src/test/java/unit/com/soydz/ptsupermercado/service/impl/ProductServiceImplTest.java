13348ae20763ccb1dc934b8457c5d42a8f852e39
soydz <dev@soydz.com>
2025-12-17 20:27:31 -0500

    test(product): add unit test for ProductServiceImpl

---
+++ src/test/java/unit/com/soydz/ptsupermercado/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.soydz.ptsupermercado.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import com.soydz.ptsupermercado.dto.ProductReqDTO;
+import com.soydz.ptsupermercado.dto.ProductResDTO;
+import com.soydz.ptsupermercado.entity.Product;
+import com.soydz.ptsupermercado.entity.Supplier;
+import com.soydz.ptsupermercado.repository.IProductRepository;
+import com.soydz.ptsupermercado.service.interfaces.ISupplierService;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceImplTest {
+
+  @Mock private IProductRepository productRepository;
+
+  @Mock private ISupplierService supplierService;
+
+  @InjectMocks private ProductServiceImpl productService;
+
+  private Product product;
+  private ProductReqDTO productReqDTO;
+
+  @BeforeEach
+  void setup() {
+    Supplier supplier = new Supplier();
+    supplier.setId(1L);
+    supplier.setName("Granos del tolima");
+    supplier.setEmail("clientes@granosdeltolima.com");
+
+    productReqDTO =
+        new ProductReqDTO("Arroz la abundancia", "Granos", new BigDecimal(1850), supplier.getId());
+
+    product = ProductReqDTO.toEntity(productReqDTO, supplier);
+  }
+
+  @Test
+  void shouldReturnProductWhenSaveIsSuccessful() {
+    // When
+    when(productRepository.save(any(Product.class))).thenReturn(product);
+
+    ProductResDTO result = productService.save(productReqDTO);
+
+    // Then
+    assertNotNull(result);
+    assertEquals(
+        "Arroz la abundancia", result.name(), "The product name does not match the expected value");
+    assertEquals(
+        "Granos del tolima",
+        result.supplierName(),
+        "The supplier name does not match the expected value");
+
+    verify(productRepository, times(1)).save(any(Product.class));
+  }
+}
