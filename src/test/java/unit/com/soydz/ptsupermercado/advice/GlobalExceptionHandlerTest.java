4f5031db2a434136237ccb459eca3d714806dac5
soydz <dev@soydz.com>
2025-12-18 16:27:20 -0500

    test(advice): add unit test for GlobalExceptionHandler

---
+++ src/test/java/unit/com/soydz/ptsupermercado/advice/GlobalExceptionHandlerTest.java
@@ -0,0 +1,64 @@
+package com.soydz.ptsupermercado.advice;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.soydz.ptsupermercado.service.impl.ProductServiceImpl;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class GlobalExceptionHandlerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockitoBean private ProductServiceImpl productService;
+
+  @Test
+  void shouldReturn400WhenMethodArgumentNotValidExceptionOccurs() throws Exception {
+
+    String body =
+        """
+        {
+            "name":""
+        }
+        """;
+
+    // Then
+    mockMvc
+        .perform(post("/api/v1/productos").contentType(MediaType.APPLICATION_JSON).content(body))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message").value("Validation failed"))
+        .andExpect(jsonPath("$.errors[?(@.field=='name')].message").value("must not be blank"))
+        .andExpect(jsonPath("$.errors[?(@.field=='price')].message").value("must not be null"));
+  }
+
+  @Test
+  void shouldReturn404WhenEntityNotFoundExceptionOccurs() throws Exception {
+    // When
+    when(productService.save(any())).thenThrow(new EntityNotFoundException());
+
+    String body =
+        """
+        {
+          "name": "Arroz",
+          "category": "Grano",
+          "price": 500,
+          "supplierId": 4768464
+        }
+      """;
+
+    // Then
+    mockMvc
+        .perform(post("/api/v1/productos").contentType(MediaType.APPLICATION_JSON).content(body))
+        .andExpect(status().isNotFound());
+  }
+}
