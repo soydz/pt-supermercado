e3c5b276c43ba190515625817f9d5f4a75577dc3
soydz <dev@soydz.com>
2025-12-27 17:11:26 -0500

    test(product): add integration test for PostgreSQL persistence

--- src/test/java/integration/com/soydz/ptsupermercado/repository/ProductRepositoryIT.java
+++ src/test/java/integration/com/soydz/ptsupermercado/repository/ProductRepositoryIT.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
@@ -18,6 +19,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @DataJpaTest
+@ActiveProfiles("test")
 class ProductRepositoryIT {
 
   @Container
