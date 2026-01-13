3e2c984aa4f03a41a26a4ccc05fbaf27f2c3a256
soydz <dev@soydz.com>
2026-01-13 11:08:15 -0500

    feat(store): implement update store endpoint and corresponding tests

--- src/main/java/com/soydz/ptsupermercado/service/impl/StoreServiceImpl.java
+++ src/main/java/com/soydz/ptsupermercado/service/impl/StoreServiceImpl.java
@@ -2,8 +2,10 @@ package com.soydz.ptsupermercado.service.impl;
 
 import com.soydz.ptsupermercado.dto.StoreReqDTO;
 import com.soydz.ptsupermercado.dto.StoreResDTO;
+import com.soydz.ptsupermercado.entity.Store;
 import com.soydz.ptsupermercado.repository.IStoreRepository;
 import com.soydz.ptsupermercado.service.exception.StoreDuplicateNameException;
+import com.soydz.ptsupermercado.service.exception.StoreNotFoundException;
 import com.soydz.ptsupermercado.service.interfaces.IStoreService;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -29,5 +31,21 @@ public class StoreServiceImpl implements IStoreService {
   @Override
   public List<StoreResDTO> findAll() {
     return storeRepository.findAll().stream().map(StoreResDTO::fromEntity).toList();
+  }
+
+  @Override
+  public Store findById(Long id) {
+    return storeRepository.findById(id).orElseThrow(() -> new StoreNotFoundException(id));
+  }
+
+  @Override
+  public StoreResDTO update(StoreReqDTO storeReqDTO, Long id) {
+    Store store = findById(id);
+
+    store.setName(storeReqDTO.name());
+    store.setAddress(storeReqDTO.name());
+    store.setCity(storeReqDTO.city());
+
+    return StoreResDTO.fromEntity(storeRepository.save(store));
   }
 }
--- src/main/java/com/soydz/ptsupermercado/service/interfaces/IStoreService.java
+++ src/main/java/com/soydz/ptsupermercado/service/interfaces/IStoreService.java
@@ -2,6 +2,7 @@ package com.soydz.ptsupermercado.service.interfaces;
 
 import com.soydz.ptsupermercado.dto.StoreReqDTO;
 import com.soydz.ptsupermercado.dto.StoreResDTO;
+import com.soydz.ptsupermercado.entity.Store;
 import java.util.List;
 
 public interface IStoreService {
@@ -9,4 +10,8 @@ public interface IStoreService {
   StoreResDTO save(StoreReqDTO storeReqDTO);
 
   List<StoreResDTO> findAll();
+
+  Store findById(Long id);
+
+  StoreResDTO update(StoreReqDTO storeReqDTO, Long id);
 }
